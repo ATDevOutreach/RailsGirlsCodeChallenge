34a4bdd664621abc0f1ca7e887c6108a566f3cd0
GettyOrawo <gettyorawo@gmail.com>
2018-09-20 17:16:00 +0300

    Can Create New Contact

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,21 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+  def new
+    @contact = Contact.new
+  end
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :phonenumber)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+print '.'
+  Contact.create!(first_name: "Gety", last_name: "orao", phonenumber: "254711906665")
+end
