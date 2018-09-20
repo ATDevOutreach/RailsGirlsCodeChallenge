da82ec6d16bdbd545bd35a939bb3d51261b0599a
GettyOrawo <gettyorawo@gmail.com>
2018-09-20 18:12:21 +0300

    Add error message on submission

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  validates :first_name, :last_name, :phonenumber, presence: true
+  validates :phonenumber, length: { maximum: 12 }
+  validates_numericality_of :phonenumber
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_09_20_132335) do
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "phonenumber"
+    t.integer "phonenumber"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
