4075b653da0fc1ce1c40d9843908086bb3f6e1d4
GettyOrawo <gettyorawo@gmail.com>
2018-09-26 16:05:00 +0300

    Add fash message

--- app/controllers/sms_sends_controller.rb
+++ app/controllers/sms_sends_controller.rb
@@ -1,4 +1,3 @@
-require './gateway.rb'
 require './AfricasTalkingGateway'
 class SmsSendsController < ApplicationController
   def new
@@ -8,8 +7,10 @@ class SmsSendsController < ApplicationController
   def create
     @at = SmsSend.create(sms_params)
     if @at.save!
-    @con = current_user.contacts.find_by(id: @at.contact_id)
-    AfricasTalkingGateway.new("trial-error", "29f7557bfacfe4335ea850fc3fe258b7f70064d069a014e39d5d2eece16659a9").sendMessage(@con.phonenumber, @at.message)
+      @con = current_user.contacts.find_by(id: @at.contact_id)
+      AfricasTalkingGateway.new("trial-error", "29f7557bfacfe4335ea850fc3fe258b7f70064d069a014e39d5d2eece16659a9").sendMessage(@con.phonenumber, @at.message)
+      redirect_to contacts_path
+      flash[:success] = "Message Successfully sent to #{@con.first_name}!"
     end
   end
   def new_all
@@ -19,9 +20,11 @@ class SmsSendsController < ApplicationController
   def create_all
     @at = SmsSend.create(sms_params)
     if @at.save!
-    current_user.contacts.each do |c|
-      AfricasTalkingGateway.new("trial-error", "29f7557bfacfe4335ea850fc3fe258b7f70064d069a014e39d5d2eece16659a9").sendMessage(c.phonenumber, @at.message)
-    end
+      current_user.contacts.each do |c|
+        AfricasTalkingGateway.new("trial-error", "29f7557bfacfe4335ea850fc3fe258b7f70064d069a014e39d5d2eece16659a9").sendMessage(c.phonenumber, @at.message)
+      end
+      redirect_to contacts_path
+      flash[:success] = "Message Successfully sent to all your contacts!"
     end
   end
 
